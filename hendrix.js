3318c27e471dd3b13a84c7e04ca5424a3c5e1d63
Mircea Moise <mmoise@thoughtworks.com>
2015-08-05 00:22:09 +0100

    Crude implementation of auto-save

---
+++ hendrix.js
@@ -0,0 +1,28 @@
+var hendrix = hendrix || {};
+
+hendrix.editor = function($panel) {
+  function save() {
+    if (history[history.length - 1] !== $panel.text()) {
+      history.push($panel.text());
+    }
+    console.log(history);
+  }
+
+  var history = [""];
+  save();
+
+  return {
+    save: save
+  }
+
+}
+
+hendrix.controller = function() {
+  var editor = hendrix.editor($(".panel"));
+
+  setInterval(function(){
+    editor.save();
+  }, 10000);
+}
+
+hendrix.controller();
