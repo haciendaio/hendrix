c93c205d84c39ac7c5e8c28e74efe4e48cf1c365
Mircea Moise <mmoise@thoughtworks.com>
2015-08-05 22:09:59 +0100

    Added an undo button

--- app/hendrix.js
+++ app/hendrix.js
@@ -12,21 +12,35 @@ hendrix.editor = function($panel) {
     console.log(history);
   }
 
+  function undo() {
+    var currentText = $panel.text();
+    if (_.last(history) === currentText) {
+      history.pop();
+    }
+    $panel.text(history.pop())
+  }
+
   var history = [];
   save();
 
   return {
-    save: save
+    save: save,
+    undo: undo
   }
 
 }
 
 hendrix.controller = function() {
   var editor = hendrix.editor($(".panel"));
+  var $undoButton = $("#undo");
+
+  $undoButton.click(function(){
+    editor.undo();
+  });
 
   setInterval(function(){
     editor.save();
-  }, 10000);
+  }, 5000);
 }
 
 hendrix.controller();
