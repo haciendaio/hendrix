4f2376212163eba58743c087b3c51cf5711e076f
Mircea Moise <mmoise@thoughtworks.com>
2015-08-08 19:18:05 +0100

    Added ability to recreate the bundle after each change in js

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var gulp = require('gulp');
+var watch = require('gulp-watch');
 
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 
 gulp.task('default', function () {
     console.log('default task');
-    return browserify('./app/hendrix.js')
-    .bundle()
-    .pipe(source('hendrix-bundle.js'))
-    .pipe(gulp.dest('./build/'))
+    gulp.watch('./app/hendrix.js', ['browserify']);
+});
+
+gulp.task('browserify', function() {
+  console.log('browserify')
+  return browserify('./app/hendrix.js')
+  .bundle()
+  .pipe(source('hendrix-bundle.js'))
+  .pipe(gulp.dest('./build/'))
 });
